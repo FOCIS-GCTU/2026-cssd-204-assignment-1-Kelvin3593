d3d6447ecdcc47c30f323275bb4bed3495ce6dd1
Kelvin3593 <2425403593@live.gctu.edu.gh>
2026-05-31 20:02:27 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,41 @@
+# File: initials.py
+# Description: To print out my initials.
+# Assignment Number: 1
+#
+# Name: Kelvin Ampofo
+# STUDENT ID: 2425403593
+# Email: 2425403593@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Kelvin Ampofo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This function prints the initials in stylized large letters.
+
+    # Blank line before small initials
+    print()
 
 
+    # Small initials
+    print("...KKA")
+
+    # Blank line before large initials
+    print()
+
+    # Large initials (KKA)
+    print("...KKKK....KKKK........KKKK....KKKK..............A..........")
+    print("...KKKK...KKKK.........KKKK...KKKK..............AAA.........")
+    print("...KKKK..KKKK..........KKKK..KKKK...............AAA.........")
+    print("...KKKK.KKKK...........KKKK.KKKK...............AA.AA........")
+    print("...KKKKKKKK............KKKKKKKK...............AA...AA.......")
+    print("...KKKKKKKK............KKKKKKKK...............AA...AA.......")
+    print("...KKKK.KKKK...........KKKK.KKKK.............AAAAAAAAA......")
+    print("...KKKK..KKKK..........KKKK..KKKK...........AAAAAAAAAAA.....")
+    print("...KKKK...KKKK....**...KKKK...KKKK....**....AAAA...AAAA...**")
+    print("...KKKK....KKKK...**...KKKK....KKKK...**...AAAAA...AAAA...**")
+    # Blank line after large initials
+    print()
 
 main()
